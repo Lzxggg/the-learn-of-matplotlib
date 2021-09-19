de1489c1078f5b08b928c815c51cb0ea7a7d487a
Lzxggg <470939734@qq.com>
2021-09-19 20:56:44 +0800

    second

--- test.py
+++ test.py
@@ -24,6 +24,72 @@ import numpy as np
 # plt.show()
 
 #第三节课 修改坐标轴之间的间隔 坐标轴名称  转成英文坐标轴而不是 数值   正则表达式转数学形式  ......
+# x = np.linspace(-3,3,50)
+# y1 = x**2
+# y2 = 2*x+1
+
+# plt.figure(num=3,figsize=(8,5))
+# plt.plot(x,y2)
+# plt.plot(x,y1,color='red',linewidth=1.0,linestyle='--')
+# plt.xlim((-1,2))
+# plt.ylim((-2,3))
+# plt.xlabel('x x')
+# plt.ylabel('y y')
+# new_ticks = np.linspace(-1,2,5) #-1,2分五个单位
+# print(new_ticks)
+# plt.xticks(new_ticks)
+# plt.yticks([-2,-1.8,-1,1.22,3],
+#             [r'$really\ bad$',r'$bad \ \alpha$',r'$normal$',r'$good$',r'$very\ good$'])
+# # 这里采取正则表达式的形式 将原有的英文形式 转换为 数学形式  注意\alpha  关联latex
+
+# plt.figure(num=4,figsize=(8,5))
+# plt.plot(x,y2)
+# plt.plot(x,y1,color='green',linewidth=1.0,linestyle='--')
+# plt.xlim((-1,2))
+# plt.ylim((-2,3))
+# plt.xlabel('x x')
+# plt.ylabel('y y')
+# new_ticks = np.linspace(-1,2,5) #-1,2分五个单位
+# print(new_ticks)
+# plt.xticks(new_ticks)
+# plt.yticks([-2,-1.8,-1,1.22,3],
+#             ['really bad','bad','normal','good','very good'])
+# plt.show()
+
+#第四节课 修改坐标轴之间的间隔 坐标轴名称  转成英文坐标轴而不是 数值   正则表达式转数学形式  ......
+# x = np.linspace(-3,3,50)
+# y1 = x**2
+# y2 = 2*x+1
+
+# plt.figure(num=3,figsize=(8,5))
+# plt.plot(x,y2)
+# plt.plot(x,y1,color='red',linewidth=1.0,linestyle='--')
+# plt.xlim((-1,2))
+# plt.ylim((-2,3))
+# plt.xlabel('x x')
+# plt.ylabel('y y')
+# new_ticks = np.linspace(-1,2,5) #-1,2分五个单位
+# print(new_ticks)
+# plt.xticks(new_ticks)
+# plt.yticks([-2,-1.8,-1,1.22,3],
+#             [r'$really\ bad$',r'$bad \ \alpha$',r'$normal$',r'$good$',r'$very\ good$'])
+# # 这里采取正则表达式的形式 将原有的英文形式 转换为 数学形式  注意\alpha  关联latex
+
+# plt.figure(num=4,figsize=(8,5))
+# plt.plot(x,y2)
+# plt.plot(x,y1,color='green',linewidth=1.0,linestyle='--')
+# plt.xlim((-1,2))
+# plt.ylim((-2,3))
+# plt.xlabel('x x')
+# plt.ylabel('y y')
+# new_ticks = np.linspace(-1,2,5) #-1,2分五个单位
+# print(new_ticks)
+# plt.xticks(new_ticks)
+# plt.yticks([-2,-1.8,-1,1.22,3],
+#             ['really bad','bad','normal','good','very good'])
+# plt.show()
+
+#第五节课 修改坐标轴之间的间隔 坐标轴名称  转成英文坐标轴而不是 数值   正则表达式转数学形式  ......
 x = np.linspace(-3,3,50)
 y1 = x**2
 y2 = 2*x+1
@@ -41,17 +107,13 @@ plt.xticks(new_ticks)
 plt.yticks([-2,-1.8,-1,1.22,3],
             [r'$really\ bad$',r'$bad \ \alpha$',r'$normal$',r'$good$',r'$very\ good$'])
 # 这里采取正则表达式的形式 将原有的英文形式 转换为 数学形式  注意\alpha  关联latex
-
-plt.figure(num=4,figsize=(8,5))
-plt.plot(x,y2)
-plt.plot(x,y1,color='green',linewidth=1.0,linestyle='--')
-plt.xlim((-1,2))
-plt.ylim((-2,3))
-plt.xlabel('x x')
-plt.ylabel('y y')
-new_ticks = np.linspace(-1,2,5) #-1,2分五个单位
-print(new_ticks)
-plt.xticks(new_ticks)
-plt.yticks([-2,-1.8,-1,1.22,3],
-            ['really bad','bad','normal','good','very good'])
+#gca 'get current axis'
+ax = plt.gca()
+#取消右边 上边的边
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+ax.spines['bottom'].set_position(('data',-1)) 
+ax.spines['left'].set_position(('data',2))
 plt.show()
